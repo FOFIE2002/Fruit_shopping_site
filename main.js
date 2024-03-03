c0f8a5c421686beb42d7c306b5296a89b8f49cae
Fofie Fopa Elisabeth <fofiefopaelisabeth@gmail.com>
2024-03-03 17:44:27 +0100

    added swipper autoplay

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     // pagination: {
     //   el: ".swiper-pagination",
     //   clickable: true,
